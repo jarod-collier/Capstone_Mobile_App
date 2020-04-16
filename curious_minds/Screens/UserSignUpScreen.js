9074c844856ac8b35502242ffbc657f03eb7cce7
shinjimasuka <collierj@mail.gvsu.edu>
2020-04-16 19:08:06 -0400

    Fixed the user sign up screen

--- curious_minds/Screens/UserSignUpScreen.js
+++ curious_minds/Screens/UserSignUpScreen.js
@@ -19,106 +19,110 @@ import {
   Alert,
 } from 'react-native';
 
-var state = {
-  FirstName: '',
-  LastName: '',
-  Username: '',
-  Password: '',
-  Email: '',
-};
-// There has to be something in this last string otherwise a classic undefined is not an object. no idea why
-// looks like if you refresh the whole thing then it works fine. Once you try to update
-// this specific page while editing it then it gets wonky
 
-var handleFirstName = text => {
+const handleFirstName = text => {
   state.FirstName = text;
 };
-var handleLastName = text => {
+const handleLastName = text => {
   state.LastName = text;
 };
-var handleUsername = text => {
+const handleUsername = text => {
   state.Username = text;
 };
-var handlePassword = text => {
+const handlePassword = text => {
   state.Password = text;
 };
-var handleEmail = text => {
+const handleEmail = text => {
   state.Email = text;
 };
 
-async function checkUsername(){
-  let usernames = [];
-  await db.ref('/userInfo/').once('value', function(snapshot){
-    snapshot.forEach((child) => {
+const clearFirstName = React.createRef();
+const clearLastName = React.createRef();
+const clearUsername = React.createRef();
+const clearPassword = React.createRef();
+const clearEmail = React.createRef();
+
+// useFocusEffect(
+  //   React.useCallback(() => {
+    //     // Do something when the screen is focused
+    //     return () => {
+      //       // Do something when the screen is unfocused
+      //       clearFirstName.current.clear();
+      //       clearLastName.current.clear();
+      //       clearUsername.current.clear();
+      //       clearPassword.current.clear();
+      //       clearEmail.current.clear();
+      //     };
+      //   }, [])
+      // );
+
+export default class UserSignUpScreen extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      FirstName: '',
+      LastName: '',
+      Username: '',
+      Password: '',
+      Email: '',
+    };
+  }
+
+  async handleSignUp(navigation){
+    const valid = await this.checkUsername();
+    if(valid){
+      var UserId;
+      firebase.auth()
+      .createUserWithEmailAndPassword(state.Email, state.Password)
+      .then(data => UserId = data.user.uid)
+      .then(() => db.ref('/userInfo').push({
+        First: "" + state.FirstName,
+        Last: "" + state.LastName,
+        Username: "" + state.Username,
+        uid: UserId,
+        postNum: 0,
+        commentNum: 0,
+        AddintionalInfo: "",
+        score: 0,
+        userType: "user"
+      }).catch((error)=>{
+        Alert.alert('error ', error)
+      }))
+      .then(() => navigation.reset({
+        index: 0,
+        routes: [{ name: 'Main'}],
+      }))
+      .catch(error => Alert.alert(error.message));
+    }
+  };
+
+  async checkUsername(){
+    let usernames = [];
+    await db.ref('/userInfo/').once('value', function(snapshot){
+      snapshot.forEach((child) => {
         usernames.push(
-            child.val().Username
+          child.val().Username
         );
-    })
-});
-  if(usernames.includes(state.Username)){
-    Alert.alert('username is already in use\nPlease try a different username');
-    return false;
-  }else{
-    return true;
+      })
+    });
+    if(usernames.includes(state.Username)){
+      Alert.alert('username is already in use\nPlease try a different username');
+      return false;
+    }else{
+      return true;
+    }
   }
-}
 
-async function handleSignUp(navigation){
-  const valid = await checkUsername();
-  if(valid){
-    var UserId;
-    firebase.auth()
-    .createUserWithEmailAndPassword(state.Email, state.Password)
-    .then(data => UserId = data.user.uid)
-    .then(() => db.ref('/userInfo').push({
-      First: "" + state.FirstName,
-      Last: "" + state.LastName,
-      Username: "" + state.Username,
-      uid: UserId,
-      postNum: 0,
-      commentNum: 0,
-      AddintionalInfo: "",
-      score: 0,
-      userType: "user"
-    }).catch((error)=>{
-      Alert.alert('error ', error)
-    }))
-    .then(() => navigation.reset({
-      index: 0,
-      routes: [{ name: 'Main'}],
-    }))
-    .catch(error => Alert.alert(error.message));
-  }
-};
-
-var clearFirstName = React.createRef();
-var clearLastName = React.createRef();
-var clearUsername = React.createRef();
-var clearPassword = React.createRef();
-var clearEmail = React.createRef();
-
-function UserSignUpScreen({navigation}) {
-  LayoutAnimation.easeInEaseOut();
-  useFocusEffect(
-    React.useCallback(() => {
-      // Do something when the screen is focused
-      return () => {
-        // Do something when the screen is unfocused
-        clearFirstName.current.clear();
-        clearLastName.current.clear();
-        clearUsername.current.clear();
-        clearPassword.current.clear();
-        clearEmail.current.clear();
-      };
-    }, [])
-  );
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <KeyboardAwareScrollView
-         resetScrollToCoords={{x: 0, y: 0}}
-         contentContainerStyle={styles.container}
-         scrollEnabled={false}
-         extraHeight={100}
+  render() {
+    LayoutAnimation.easeInEaseOut();
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <KeyboardAwareScrollView
+          resetScrollToCoords={{x: 0, y: 0}}
+          contentContainerStyle={styles.container}
+          scrollEnabled={false}
+          extraHeight={100}
         >
           <View style={styles.logo}>
             <Image source={require('../images/CM_logo02.png')} />
@@ -132,14 +136,22 @@ function UserSignUpScreen({navigation}) {
                 style={styles.namesInput}
                 placeholder="  FirstName"
                 placeholderTextColor="black"
-                onChangeText={handleFirstName}
+                onChangeText={e => {
+                      this.setState({
+                        FirstName: e,
+                      });
+                    }}
                 ref={clearFirstName}
               />
               <TextInput
                 style={styles.namesInput}
                 placeholder="  LastName"
                 placeholderTextColor="black"
-                onChangeText={handleLastName}
+                onChangeText={e => {
+                      this.setState({
+                        LastName: e,
+                      });
+                    }}
                 ref={clearLastName}
               />
             </View>
@@ -148,7 +160,11 @@ function UserSignUpScreen({navigation}) {
                 style={styles.inputBox}
                 placeholder="  Username"
                 placeholderTextColor="black"
-                onChangeText={handleUsername}
+                onChangeText={e => {
+                      this.setState({
+                        Username: e,
+                      });
+                    }}
                 ref={clearUsername}
               />
               <TextInput
@@ -157,7 +173,11 @@ function UserSignUpScreen({navigation}) {
                 secureTextEntry={true}
                 autoCapitalize='none'
                 placeholderTextColor="black"
-                onChangeText={handlePassword}
+                onChangeText={e => {
+                      this.setState({
+                        Password: e,
+                      });
+                    }}
                 ref={clearPassword}
               />
               <TextInput
@@ -165,7 +185,11 @@ function UserSignUpScreen({navigation}) {
                 placeholder="  Email"
                 keyboardType='email-address'
                 placeholderTextColor="black"
-                onChangeText={handleEmail}
+                onChangeText={e => {
+                      this.setState({
+                        Email: e,
+                      });
+                    }}
                 ref={clearEmail}
               />
             </View>
@@ -173,13 +197,14 @@ function UserSignUpScreen({navigation}) {
           <View>
             <TouchableOpacity
               style={styles.Buttons}
-              onPress={() => handleSignUp(navigation)}>
+              onPress={() => this.handleSignUp(this.props.navigation)}>
               <Text style={styles.customBtnText}>Sign Up</Text>
             </TouchableOpacity>
           </View>
         </KeyboardAwareScrollView>
-    </SafeAreaView>
-  );
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -243,4 +268,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default UserSignUpScreen;
+// export default UserSignUpScreen;
